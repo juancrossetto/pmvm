175dba02f536263f7cafa2a9ec01e3f07d0f6c2a
Juanma Crossetto <juancrossetto@gmail.com>
2024-08-24 16:59:34 -0300

    feat: ajustes con menu responsive del header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,22 +1,25 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
-import { DumbbellIcon } from "lucide-react";
+import { DumbbellIcon, Menu, X } from "lucide-react";
 import LanguageSelector from "./language-selector";
 import { useTranslations } from "next-intl";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import {
+	Sheet,
+	SheetClose,
+	SheetContent,
+	SheetTrigger,
+} from "@/components/ui/sheet";
 
 const Header = () => {
-	const t = useTranslations("header");
 	return (
 		<>
-			<header className='header'>
-				<Link
-					// className='flex items-center justify-center'
-					className='logo'
-					href='#'
-				>
+			<header
+			// className='header'
+			>
+				{/* <Link className='logo' href='#'>
 					<Avatar className='h-12 w-12'>
 						<AvatarImage
 							className='bg-white'
@@ -25,20 +28,125 @@ const Header = () => {
 						/>
 						<AvatarFallback>PMVM</AvatarFallback>
 					</Avatar>
-				</Link>
-				{/* <a href="#" className="logo">PMVM</a> */}
-				<nav className='navbar'>
-					<a href='#services' className='active'>{t("services")}</a>
+				</Link> */}
+				<Navbar />
+				{/* <nav className='navbar'>
+					<a href='#services' className='active'>
+						{t("services")}
+					</a>
 					<a href='#testimonials'>{t("testimonials")}</a>
 					<a href='#contact'>{t("contact")}</a>
 				</nav>
 				<div className='language-selector-container'>
 					<LanguageSelector />
 				</div>
-				<div className="div-empty"/>
+				<div className='div-empty' /> */}
 			</header>
 		</>
 	);
 };
 
 export default Header;
+
+const Navbar = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const t = useTranslations("header");
+	// const [currentLanguage, setCurrentLanguage] = useState('es')
+	const navItems = [
+		{ name: "services", href: "#services" },
+		{ name: "testimonials", href: "#testimonials" },
+		{ name: "contact", href: "#contact" },
+	];
+	return (
+		<nav className='navbar shadow-md'>
+			<div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+				<div className='flex justify-between h-16 relative z-[1]'>
+					<div className='flex-shrink-0 flex items-center'>
+						<Logo className='logo' />
+					</div>
+
+					<div className='hidden sm:flex sm:items-center sm:justify-center flex-1'>
+						{navItems.map((item) => (
+							<Link
+								key={item.name}
+								href={item.href}
+								className='px-3 py-2 rounded-md text-sm font-medium'
+							>
+								{t(item.name)}
+							</Link>
+						))}
+					</div>
+
+					{/* Language Selector (Desktop) */}
+					<div className='hidden sm:flex sm:items-center'>
+						<LanguageSelector />
+					</div>
+
+					{/* Mobile menu button */}
+					<div className='sm:hidden flex items-center z-[1]'>
+						<Sheet open={isOpen} onOpenChange={setIsOpen}>
+							<SheetTrigger asChild>
+								<Button
+									variant='ghost'
+									className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-[#facb1a]'
+									onClick={() => setIsOpen(!isOpen)}
+								>
+									<span className='sr-only'>Abrir menú principal</span>
+									<Menu className='h-6 w-6' aria-hidden='true' />
+								</Button>
+							</SheetTrigger>
+							<SheetContent
+								side='right'
+								className='w-[400px] sm:w-[500px] bg-[#171717]'
+							>
+								<nav className='flex flex-col h-full'>
+									<div className='flex items-center justify-between mb-8 ml-2'>
+										<Logo />
+										{/* <SheetTrigger asChild>
+											<Button
+												variant='ghost'
+												className='rounded-full w-9 h-9 p-0'
+											>
+												<X className='h-6 w-6' />
+											</Button>
+										</SheetTrigger> */}
+									</div>
+									<div className='flex flex-col space-y-4 items-center'>
+										{navItems.map((item) => (
+											<Link
+												key={item.name}
+												href={item.href}
+												className='px-3 py-2 rounded-md text-base font-medium text-white'
+												onClick={() => setIsOpen(!isOpen)}
+											>
+												{t(item.name)}
+											</Link>
+										))}
+									</div>
+									<div className='mt-auto mb-8'>
+										<LanguageSelector />
+									</div>
+								</nav>
+							</SheetContent>
+						</Sheet>
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+const Logo = ({ className }: { className?: string }) => {
+	return (
+		<Link className={className} href='#'>
+			<Avatar className='h-12 w-12'>
+				<AvatarImage
+					className='bg-white'
+					src={`/images/icon.webp`}
+					alt={"Pesá menos, viví más"}
+				/>
+				<AvatarFallback>PMVM</AvatarFallback>
+			</Avatar>
+		</Link>
+	);
+};
