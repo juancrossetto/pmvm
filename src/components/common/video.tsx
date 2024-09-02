340af99b404a9a0cc118d81cf515f315786653e0
Juanma Crossetto <juancrossetto@gmail.com>
2024-09-02 11:11:40 -0300

    feat: ajuste de video para autoplay en mobile

--- src/components/common/video.tsx
+++ src/components/common/video.tsx
@@ -1,78 +1,82 @@
-/* eslint-disable react/display-name */
-import { forwardRef, useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 interface VideoProps {
-	src: string;
-	id?: string;
-	controls?: boolean;
-	style?: React.CSSProperties;
-	className?: string;
-	poster?: string;
-	autoPlay?: boolean;
-	preload?: "auto" | "metadata" | "none";
-	fallbackImage: string; // Nueva prop para la imagen de fallback
+  src: string;
+  id?: string;
+  controls?: boolean;
+  style?: React.CSSProperties;
+  className?: string;
+  poster?: string;
+  autoPlay?: boolean;
+  preload?: "auto" | "metadata" | "none";
+  fallbackImage: string;
 }
 
-const Video = forwardRef(
-	(
-		{
-			src,
-			autoPlay = true,
-			preload = "auto",
-			fallbackImage,
-			...rest
-		}: VideoProps,
-		ref: any
-	) => {
-		const [showFallback, setShowFallback] = useState(false);
+const Video: React.FC<VideoProps> = ({
+  src,
+  autoPlay = true,
+  preload = "auto",
+  fallbackImage,
+  ...rest
+}) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [showFallback, setShowFallback] = useState(false);
 
-		useEffect(() => {
-			const videoElement = ref?.current;
+  useEffect(() => {
+    const videoElement = videoRef.current;
 
-			try {
-				if (videoElement) {
-					videoElement.muted = true;
-					if (autoPlay) {
-						videoElement.play().catch((error: any) => {
-							console.error("AutoPlay error:", error);
-							setShowFallback(true); // Muestra la imagen de fallback si falla el autoplay
-						});
-					}
-				} else {
-					// setShowFallback(true); // Muestra la imagen si el videoElement es null o undefined
-				}
-			} catch (error) {
-				setShowFallback(true);
-			}
-		}, [ref, autoPlay]);
+    if (videoElement) {
+      videoElement.muted = true;
 
-		if (showFallback) {
-			return (
-				<img
-					src={fallbackImage}
-					alt='Fallback'
-					className={rest.className}
-					style={rest.style}
-				/>
-			);
-		}
+      const handlePlay = () => {
+        videoElement.play().catch((error) => {
+          console.error("AutoPlay error:", error);
+          setShowFallback(true); // Muestra la imagen si falla el autoplay
+        });
+      };
 
-		return (
-			<video
-				ref={ref}
-				playsInline
-				autoPlay={autoPlay}
-				muted
-				loop
-				preload={preload}
-				onCanPlay={(e) => e.currentTarget.play()}
-				{...rest}
-			>
-				<source src={src} type='video/mp4' />
-				Your browser does not support the video tag.
-			</video>
-		);
-	}
-);
+      // Intentar reproducir el video
+      if (autoPlay) {
+        handlePlay();
+      }
+
+      // Listener para detectar si el video falla en reproducirse
+      videoElement.addEventListener("error", () => {
+        setShowFallback(true); // Mostrar imagen de fallback si hay error en la reproducción
+      });
+
+      // Limpiar el listener al desmontar el componente
+      return () => {
+        videoElement.removeEventListener("error", handlePlay);
+      };
+    }
+  }, [src, autoPlay]);
+
+  if (showFallback) {
+    return (
+      <img
+        src={fallbackImage}
+        alt="Fallback"
+        className={rest.className}
+        style={rest.style}
+      />
+    );
+  }
+
+  return (
+    <video
+      ref={videoRef}
+      playsInline
+      autoPlay={autoPlay}
+      muted
+      loop
+      preload={preload}
+      {...rest}
+    >
+      <source src={src} type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+  );
+};
 
 export default Video;
